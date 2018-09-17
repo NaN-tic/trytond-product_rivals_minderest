a3624d244d792a4b7b78eb023dd84b1e64f4cd2c
Raimon Esteve <raimon@nan-tic.com>
2018-09-17 18:06:41 +0200

    Add miniderest recommended price #033273

--- rivals.py
+++ rivals.py
@@ -5,13 +5,10 @@ import urllib2
 import base64
 import csv
 from decimal import Decimal
-from trytond.config import config
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 
 __all__ = ['ProductAppRivals']
-
-DIGITS = config.getint('product', 'price_decimal', default=4)
 
 
 class ProductAppRivals:
@@ -21,8 +18,7 @@ class ProductAppRivals:
     @classmethod
     def __setup__(cls):
         super(ProductAppRivals, cls).__setup__()
-
-        required = (Eval('app') == 'minderest')
+        required = (Eval('app') in ['minderest', 'minderest_recommended_price'])
         if 'required' in cls.app_username.states:
             cls.app_username.states['required'] |= required
         else:
@@ -35,6 +31,7 @@ class ProductAppRivals:
     @classmethod
     def get_app(cls):
         res = super(ProductAppRivals, cls).get_app()
+        res.append(('minderest_recommended_price', 'Minderest Recommended Price'))
         res.append(('minderest', 'Minderest'))
         return res
 
@@ -89,3 +86,63 @@ class ProductAppRivals:
                 }
 
         self.create_rivals(values)
+
+    def update_prices_minderest_recommended_price(self):
+        username = self.app_username
+        password = self.app_password
+        request = urllib2.Request(self.app_uri)
+        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
+        result = urllib2.urlopen(request)
+        rows = result.read().decode('iso-8859-1').encode('utf8')
+        self.minderest_recommended_prices(rows)
+
+    def minderest_recommended_prices(self, rows):
+        RecommendedPrice = Pool().get('product.minderest_recommended_price')
+        has_header = True
+        csv_dict = {}
+        to_save = []
+
+        spamreader = csv.reader(rows.split('\n'), delimiter=';')
+        field_names_list = spamreader.next()
+
+        for row in csv.reader(rows.split('\n'), delimiter=';'):
+            if not row:
+                continue
+            for i in xrange(0,len(field_names_list)):
+                csv_dict[field_names_list[i]] = row[i]
+            if has_header:
+                has_header = False
+                continue
+
+            recommended_price = self.get_recomended_price(csv_dict)
+            if recommended_price:
+                to_save.append(recommended_price)
+
+        RecommendedPrice.save(to_save)
+
+    def recommended_price_domain(self, product):
+        return [('product', '=', product)]
+
+    def get_recomended_price(self, csv_dict):
+        pool = Pool()
+        RecommendedPrice = pool.get('product.minderest_recommended_price')
+        Product = Pool().get('product.product')
+
+        products = Product.search([('code', '=', str(csv_dict['g:mpn']))], limit=1)
+        if not products:
+            return
+        product, = products
+
+        recommended_prices = RecommendedPrice.search(
+            self.recommended_price_domain(product), limit=1)
+        price = Decimal(csv_dict[
+            'price_recommended_tax_excluded']).quantize(Decimal(str(10**-2)))
+        if recommended_prices:
+            recommended_price, = recommended_prices
+            recommended_price.price = price
+        else:
+            recommended_price = RecommendedPrice()
+            recommended_price.product = product
+            recommended_price.price = price
+        return recommended_price
