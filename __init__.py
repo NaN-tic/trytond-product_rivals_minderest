a25e90bd45d878a1306dbdd92e5d950c7571f0b2
Raimon Esteve <raimon@nan-tic.com>
2018-09-17 18:06:41 +0200

    Add miniderest recommended price #033273

--- __init__.py
+++ __init__.py
@@ -3,9 +3,12 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 from . import rivals
+from . import product
 
 
 def register():
     Pool.register(
+        product.Product,
+        product.MinderestRecommendedPrice,
         rivals.ProductAppRivals,
         module='product_rivals_minderest', type_='model')
